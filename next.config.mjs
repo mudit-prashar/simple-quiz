cb535273a98ed6f0f400c6d2ace2a52f5cd73b7d
Mudit Prashar <prasharmudit@gmail.com>
2024-02-09 14:49:31 -0500

    Basic Functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    experimental:{
+        instrumentationHook: true
+    }
+};
 
 export default nextConfig;
